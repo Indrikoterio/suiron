1b3cd657f1a1647aeda25764a0b8e0c2f07b924f
Cleve <indriko@yahoo.com>
2022-04-28 18:45:11 +0900

    Program to query the knowledge.

---
+++ query.go
@@ -0,0 +1,101 @@
+package main
+
+// Query - allows a user to query a Suiron knowledge base.
+// Usage:
+//
+// > go run query.go test/kings.txt
+//
+// The command above will load facts and rules from kings.txt,
+// and then prompt for a query with:
+//
+// Successfully loaded facts and rules from test/kings.txt
+// ?- 
+//
+// To find the grandchildren of Godwin, the user would type in
+// the following query:
+//
+// ?- grandfather(Godwin, $X)
+//
+// ($X is a variable.)
+//
+// The Suiron inference engine will output one result after each
+// press of 'enter'. When solutions are exhausted, the inference
+// engine will print out 'No'.
+//
+// grandfather(Godwin, Harold)
+// grandfather(Godwin, Skule)
+// No
+// ?-
+//
+// Type <enter> to end the program.
+//
+// Cleve Lendon
+
+import (
+    . "github.com/indrikoterio/suiron/suiron"
+    "strings"
+    "errors"
+    "bufio"
+    "fmt"
+    "os"
+)
+
+func main() {
+
+    kb := KnowledgeBase{}
+
+    // Is there a file name?
+    if len(os.Args) > 1 {
+        fileName := os.Args[1]
+        if _, err := os.Stat(fileName); err == nil {
+            // Read in facts and rules.
+            err = LoadKBFromFile(kb, fileName)
+            if err != nil { 
+                fmt.Println(err.Error())
+                return
+            }
+            fmt.Printf("Successfully loaded facts and rules from %v\n", fileName)
+        } else if errors.Is(err, os.ErrNotExist) {
+            fmt.Printf("The file %v does not exist.\n", fileName)
+            return
+        }
+    } else {
+        fmt.Println("This is Suiron, an inference engine written in Go by Cleve Lendon.")
+        fmt.Println("To load knowledge> go run query rules.txt")
+    }
+
+    reader := bufio.NewReader(os.Stdin)
+    previous := ""   // Previouse query.
+
+    for {
+
+        fmt.Print("?- ")  // Prompt for query.
+
+        q, _ := reader.ReadString('\n')
+        query := strings.TrimSpace(q)
+        if len(query) == 0 { break }
+
+        if query == "." {
+            query = previous
+        } else {
+            previous = query
+        }
+
+        goal, err := ParseGoal(query)
+        if err != nil {
+            fmt.Println(err.Error())
+            continue
+        }
+        results, failure := SolveAll(goal, kb, SubstitutionSet{})
+        if len(failure) != 0 {
+            fmt.Println(failure)
+        } else {
+           for _, result := range results {
+               fmt.Print(result)
+               _, _ = reader.ReadString('\n')
+           }
+           fmt.Println("No")
+        }
+    } // for
+
+} // main
