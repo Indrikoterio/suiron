99dd25db7c7446119ede92d6af0443df3a617726
Cleve <indriko@yahoo.com>
2022-05-05 19:05:48 +0900

    A function is a unifiable term.

--- suiron/function.go
+++ suiron/function.go
@@ -13,4 +13,6 @@ type Function interface {
     RecreateVariables(VarMap) Expression
     ReplaceVariables(SubstitutionSet) Expression
     String() string
+    Unify(other Unifiable, ss SubstitutionSet) (SubstitutionSet, bool)
+    TermType() int
 }
