3e56c615d58b35863c2dd1514f6f868d75d6ad4f
Cleve <indriko@yahoo.com>
2022-05-06 18:52:32 +0900

    Test for print_list() predicate.

---
+++ test/print_list_test.go
@@ -0,0 +1,32 @@
+package main
+
+// Tests 'print_list' predicate.
+//
+// Cleve Lendon
+
+import (
+    . "github.com/indrikoterio/suiron/suiron"
+    "testing"
+    "fmt"
+)
+
+func TestPrintList(t *testing.T) {
+
+    //----------------------------------------------------
+    fmt.Println("TestPrintList: - should print:\n1, 2, 3, a, b, c")
+
+    kb := KnowledgeBase{}
+
+    head, _  := ParseComplex("print_list_test")
+    u1, _ := ParseUnify("$X = [a, b, c]")
+    u2, _ := ParseUnify("$List = [1, 2, 3 | $X]")
+    p, _  := ParseSubgoal("print_list($List)")
+    body := And(u1, u2, p)
+
+    r1 := Rule(head, body)
+    kb.Add(r1)
+
+    goal, _ := ParseGoal("print_list_test")
+    Solve(goal, kb, SubstitutionSet{})
+
+} // TestPrintList
